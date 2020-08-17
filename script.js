91e1723c504ac253e3ea622c7b1fa5d7a1fe4a02
Rafael72-hue <rafael_eliu@outlook.com>
2020-08-17 17:44:05 -0300

    Calculadora

---
+++ script.js
@@ -0,0 +1,24 @@
+var array = [];
+
+function funcao(param) {
+    var txt = document.getElementById('txt');
+    array.push(param);
+    txt.innerHTML += (param);
+}
+
+function resultado(){
+    var resultado = "";
+    for (var i = 0; i < array.length; i++){
+        resultado += array[i];
+    }
+    resultado = eval(resultado);
+    txt.innerHTML = (resultado);
+}
+
+function limpar(){
+    for(var c = 1; c < array.length; c++){
+        array = array.splice();
+    }
+    result = 0
+    txt.innerHTML = "";
+}
